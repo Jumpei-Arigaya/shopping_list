92e3e8b96dc42cd7ce392d33146b56156764ea58
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-03-26 15:31:10 +0900

    docker MUIの環境のみエラーが発生

---
+++ node/shopping_list_front/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <p>aasdas</p>
+      <Button>as</Button>
+    </div>
+  );
+}
+
+export default App;
